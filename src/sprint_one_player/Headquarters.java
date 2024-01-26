11feb433f9fe54ef8f5ee764dbb0fcd34a093446
swetha <ssrihari@pdx.edu>
2024-01-26 11:22:23 -0800

    Adding code to headquarters to build Anchors, Launcher and Carriers at certain rounds

--- src/sprint_one_player/Headquarters.java
+++ src/sprint_one_player/Headquarters.java
@@ -21,22 +21,38 @@ public class Headquarters {
         // Pick a direction to build in.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation newLoc = rc.getLocation().add(dir);
+        int roundNum = rc.getRoundNum();
+        /**
+         * For every 10 rounds, build an anchor
+         */
+        if(roundNum % 10 == 0){
         if (rc.canBuildAnchor(Anchor.STANDARD)) {
             // If we can build an anchor do it!
             rc.buildAnchor(Anchor.STANDARD);
             rc.setIndicatorString("Building anchor! " + rc.getNumAnchors(null));
         }
+    }
         if (rng.nextBoolean()) {
+            /**
+             * For every 4 rounds, create a carrier
+             */
+            if(roundNum % 4 == 0 ){
             // Let's try to build a carrier.
             rc.setIndicatorString("Trying to build a carrier");
             if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
                 rc.buildRobot(RobotType.CARRIER, newLoc);
             }
+        }
         } else {
-            // Let's try to build a launcher.
-            rc.setIndicatorString("Trying to build a launcher");
-            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
-                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            /**
+             * For every 6 rounds, create a launcher
+             */
+            if(roundNum % 6 == 0 ) {
+                // Let's try to build a launcher.
+                rc.setIndicatorString("Trying to build a launcher");
+                if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                    rc.buildRobot(RobotType.LAUNCHER, newLoc);
+                }
             }
         }
     }
