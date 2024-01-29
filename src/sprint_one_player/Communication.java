a9fa87f33d25f87d07764c3e60c5d02e1f21e4e7
james <jcwill@pdx.edu>
2024-01-28 21:11:03 -0800

    Started a new branch to test the communications array.

---
+++ src/sprint_one_player/Communication.java
@@ -0,0 +1,20 @@
+package sprint_one_player;
+
+import battlecode.common.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static sprint_one_player.RobotPlayer.directions;
+import static sprint_one_player.RobotPlayer.rng;
+
+public class Communication {
+    private static final int START_WELL_IDX = 8;
+    private static final int START_ISLAND_IDX = 20;
+    private static final int START_WELL_PRIORITY_IDX = 60;
+    private static final int XY_IDX_INCREMENT = 2;
+    private static final int WELL_IDX_INCREMENT = 3;
+
+
+}
