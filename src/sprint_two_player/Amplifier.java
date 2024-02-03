6733a1018e99162a83e4cec3f13d30803ecd7891
james <jcwill@pdx.edu>
2024-02-02 17:53:14 -0800

    Added Amplifier class.

---
+++ src/sprint_two_player/Amplifier.java
@@ -0,0 +1,12 @@
+package sprint_two_player;
+
+import battlecode.common.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static sprint_two_player.RobotPlayer.directions;
+import static sprint_two_player.RobotPlayer.rng;
+
+public class Amplifier {
+}
