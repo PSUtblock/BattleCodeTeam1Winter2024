f7cbe5969039efbe7e4d415452cd17d68942445e
james <jcwill@pdx.edu>
2024-02-12 11:15:15 -0800

    Added movement for Amplifier class to keep action radius distance away from all allied robots.

--- src/sprint_two_player/Amplifier.java
+++ src/sprint_two_player/Amplifier.java
@@ -5,10 +5,55 @@ import battlecode.common.*;
 import java.util.HashSet;
 import java.util.Set;
 
-import static sprint_two_player.RobotPlayer.directions;
-import static sprint_two_player.RobotPlayer.rng;
-
 public class Amplifier {
+    private static final int UNIT_SPACING = RobotType.AMPLIFIER.actionRadiusSquared; // Buffer spacing between amplifiers.
+
+    // Map locations to store headquarters, closest well, and island positions.
+    private static MapLocation hqLocation;
+    private static MapLocation wellLocation;
+    private static MapLocation islandLocation;
+
     public static void runAmplifier(RobotController rc) throws GameActionException {
+        MapLocation myLocation = rc.getLocation();
+
+        // If the headquarters have not already been found, locate the closest one.
+        if (hqLocation == null) {
+            hqLocation = Communication.readHQ(rc);
+        }
+
+        // If the closest well has not been found, locate it.
+        if (wellLocation == null) {
+            wellLocation = Communication.readWell(rc);
+        }
+
+        // If the closest island has not been found, locate it.
+        if (islandLocation == null) {
+            islandLocation = Communication.readIsland(rc);
+        }
+
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
+        Set<MapLocation> allyRobots = new HashSet<>();
+        MapLocation closestAlly;
+        for (RobotInfo robot : nearbyRobots) {
+            if (robot.getTeam().isPlayer()
+                    && (robot.getType() == RobotType.AMPLIFIER
+                    || robot.getType() == RobotType.CARRIER
+                    || robot.getType() == RobotType.LAUNCHER)) {
+                allyRobots.add(robot.getLocation());
+            }
+        }
+        closestAlly = Movement.getClosestLocation(rc, allyRobots);
+
+        if (closestAlly != null) {
+            if (myLocation.distanceSquaredTo(closestAlly) > UNIT_SPACING) {
+                Movement.moveToLocation(rc, closestAlly);
+            }
+            else {
+                int oppositeX = myLocation.directionTo(closestAlly).opposite().getDeltaX();
+                int oppositeY = myLocation.directionTo(closestAlly).opposite().getDeltaY();
+                MapLocation oppositeLoc = new MapLocation(myLocation.x + oppositeX, myLocation.y + oppositeY);
+                Movement.moveToLocation(rc, oppositeLoc);
+            }
+        }
     }
 }
