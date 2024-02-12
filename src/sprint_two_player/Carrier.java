7dba8e371190ba35c9bfb24f0e8d35549e78209d
james <jcwill@pdx.edu>
2024-02-11 18:20:00 -0800

    Added logical movement to stop moving randomly.

--- src/sprint_two_player/Carrier.java
+++ src/sprint_two_player/Carrier.java
@@ -2,9 +2,6 @@ package sprint_two_player;
 
 import battlecode.common.*;
 
-import static sprint_two_player.RobotPlayer.directions;
-import static sprint_two_player.RobotPlayer.rng;
-
 public class Carrier {
     // Map locations to store headquarters, closest well, and island positions.
     private static MapLocation hqLocation;
@@ -18,7 +15,6 @@ public class Carrier {
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
     public static void runCarrier(RobotController rc) throws GameActionException {
-        Direction randDir = directions[rng.nextInt(directions.length)]; // Hold rand direction if needed.
         myLocation = rc.getLocation();                                  // Get robot's current location.
 
         // If the headquarters have not already been found, locate the closest one.
@@ -48,7 +44,7 @@ public class Carrier {
             if (islandLocation != null) {
                 rc.setIndicatorString("Moving my anchor towards " + islandLocation);
                 if (!myLocation.equals(islandLocation)) {
-                    Movement.moveToLocation(rc, myLocation.directionTo(islandLocation));
+                    Movement.moveToLocation(rc, islandLocation);
                     myLocation = rc.getLocation();
                 }
                 if (rc.canPlaceAnchor()) {
@@ -59,16 +55,12 @@ public class Carrier {
                     islandLocation = null;
                 }
             }
-            else {
-                Movement.moveToLocation(rc, randDir);
-                myLocation = rc.getLocation();
-            }
         }
         // If there is capacity, then go collect resources.
         else if (rc.getWeight() < GameConstants.CARRIER_CAPACITY) {
             if (wellLocation != null) {
                 rc.setIndicatorString("Moving towards well at: " + wellLocation);
-                Movement.moveToLocation(rc, myLocation.directionTo(wellLocation));
+                Movement.moveToLocation(rc, wellLocation);
                 myLocation = rc.getLocation();
             }
             // Try to collect whether a well was located or not, in case robot has moved since located.
@@ -78,38 +70,13 @@ public class Carrier {
         else if (hqLocation != null) {
             wellLocation = null; // Reset well location if full of resources.
             if (!myLocation.isAdjacentTo(hqLocation)) {
-                Movement.moveToLocation(rc, myLocation.directionTo(hqLocation));
+                Movement.moveToLocation(rc, hqLocation);
                 myLocation = rc.getLocation();
             }
             // If next to headquarters, deposit everything from the carrier.
             depositResource(rc, ResourceType.MANA);
             depositResource(rc, ResourceType.ADAMANTIUM);
         }
-        // Otherwise, move randomly for now.
-        else {
-            Movement.moveToLocation(rc, randDir);
-            myLocation = rc.getLocation();
-        }
-
-        // Depending on certain health level, think about attacking.
-//        if (rc.getHealth() < 150) {
-//            RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-//            if (enemyRobots.length > 0) {
-//                RobotInfo enemyToAttack = enemyRobots[0];
-//                int minHealth = enemyToAttack.getHealth();
-//                // Find the enemy with the lowest health.
-//                for (int i = 1; i < enemyRobots.length; ++i) {
-//                    if (minHealth > enemyRobots[i].getHealth()) {
-//                        enemyToAttack = enemyRobots[i];
-//                        minHealth = enemyToAttack.getHealth();
-//                    }
-//                }
-//                // Try to attack the enemy.
-//                if (rc.canAttack(enemyToAttack.location)) {
-//                    rc.attack(enemyToAttack.location);
-//                }
-//            }
-//        }
     }
 
     /** Collect from well in adjacent squares. **/
